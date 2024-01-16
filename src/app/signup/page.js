b32b9e34a14a81795db567295cf4c06c460c2dc7
Deka Royanto <timoagape@gmail.com>
2024-01-16 15:13:41 +0700

    regis

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { Input, Button } from "@nextui-org/react";
 
-import { useFormik } from "formik";
+import { useFormik, Formik, Form, FieldArray, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import ImageNext from "next/image";
@@ -12,49 +12,47 @@ import Icon from "/src/assets/icons/logo-1.png";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { toastFailed, toastPending } from "@/components/ToastAlert";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import "./signup.css";
 
 export default function Page() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState("Personal");
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      username: Yup.string().required("username must be required"),
-      password: Yup.string()
-        .min(8, "Must be 8 characters or more")
-        .required("Password must be required"),
-    }),
-    onSubmit: async (e) => {
-      // alert(JSON.stringify(values, null, 2));
+  const initialValues = {
+    // id: getGenerateId(),
+    nik: "",
+    full_name: "",
+    face_value: "",
+    // card_fee: 0,
+    // max_amount: 0,
+    // effective_months: 0,
+    // unit_cost: 0,
+    business_unit: [
+      {
+        value: "",
+      },
+    ],
+  };
 
-      try {
-        setLoading(true);
-        const response = await API.post("user/login", JSON.stringify(e), {
-          headers: { "Content-Type": "application/json" },
-        });
-        console.log("success", response);
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      // Kirim data ke API
+      const response = await axios.post(`${BASE_URL}${ENDPOINT}`, values);
 
-        toastPending({
-          textPending: "Waiting..",
-          textSuccess: "Success Login",
-        });
-        setTimeout(() => {
-          router.push("dashboard");
-        }, 1000);
-      } catch (error) {
-        console.log(error);
-        setTimeout(() => {
-          toastFailed({ title: "Username / Password Salah" });
-          setLoading(false);
-        }, 1000);
-      }
-    },
-  });
+      resetForm();
+      toast.success("Account Success Created");
+      console.log("Data berhasil dikirim:", response.data);
+      onSuccess();
+      onClose();
+    } catch (error) {
+      console.error("Error saat mengirim data:", error);
+      toast.error("Gagal menambahkan data.");
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   const resolveAfter3Sec = new Promise((resolve) => setTimeout(resolve, 3000));
   const notifySuccess = () => {
@@ -74,70 +72,151 @@ export default function Page() {
       {/* <ToastContainer position="top-center" /> */}
       <div className="flex h-full overflow-hidden bg-primary rounded-3xl shadow-box-lg">
         <div className="relative flex-1 basis-[50%] bg-white flex justify-center px-5 md:px-10 lg:px-20">
-          <ImageNext
-            src={Icon}
-            alt="icon"
-            className="absolute -rotate-90 -right-10 scale-125 top-10"
-            width={150}
-          />
           <div className="w-full">
-            <h1 className="w-full text-3xl font-bold text-center text-slate-400">
+            <h1 className="w-full text-3xl font-bold text-center text-slate-400 mt-5">
               Register
             </h1>
-            <hr className="border-solid border-2 border-primary w-[15%] my-4 mx-auto" />
-            <form onSubmit={formik.handleSubmit}>
-              <Input
-                label="Username"
-                className=""
-                variant="underlined"
-                name="username"
-                placeholder="Enter your username"
-                onChange={formik.handleChange}
-                value={formik.values.username}
-              />
-              {formik.touched.username && formik.errors.username ? (
-                <div className="text-sm text-primary font-semibold">
-                  {formik.errors.username}
-                </div>
-              ) : null}
 
-              <Input
-                type="password"
-                label="Password"
-                className="mt-3"
-                variant="underlined"
-                name="password"
-                placeholder="Enter your password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <div className="text-sm font-semibold text-primary">
-                  {formik.errors.password}
-                </div>
-              ) : null}
+            <div className="flex justify-between mt-4">
+              <Button
+                variant={activeTab === "Personal" ? "flat" : "light"}
+                className={`tab ${
+                  activeTab === "Personal" ? "active-tab" : ""
+                } rounded`}
+                onClick={() => {
+                  setActiveTab("Personal");
+                  props.resetForm();
+                }}
+                fullWidth
+              >
+                Personal
+                {activeTab === "Personal" && <div className="underline"></div>}
+              </Button>
 
               <Button
-                color="primary"
-                radius="sm"
-                className="w-full mt-4 font-semibold hover:bg-secondary"
-                type="submit"
-                isLoading={loading ? true : false}
+                variant={activeTab === "Family" ? "flat" : "light"}
+                className={`tab ${
+                  activeTab === "Family" ? "active-tab" : ""
+                } rounded`}
+                onClick={() => {
+                  setActiveTab("Family");
+                  // props.resetForm();
+                }}
+                fullWidth
               >
-                Register
+                Family
+                {activeTab === "Family" && <div className="underline"></div>}
               </Button>
-              <p className="mt-5 text-center text-sm text-gray-500">
-                Already have an account?{" "}
-                <span
-                  className="font-semibold leading-6 text-black-500 hover:text-red-500 cursor-pointer"
-                  onClick={() => {
-                    router.push("/");
-                  }}
-                >
-                  Login
-                </span>
-              </p>
-            </form>
+
+              <Button
+                variant={activeTab === "Group" ? "flat" : "light"}
+                className={`tab ${
+                  activeTab === "Group" ? "active-tab" : ""
+                } rounded`}
+                onClick={() => {
+                  setActiveTab("Group");
+                  // props.resetForm();
+                }}
+                fullWidth
+              >
+                Group
+                {activeTab === "Group" && <div className="underline"></div>}
+              </Button>
+            </div>
+            <>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={Yup.object({
+                  nik: Yup.number().required("NIK code must be required"),
+                  full_name: Yup.string().required(
+                    "Full name must be required"
+                  ),
+                  face_value: Yup.string().nullable(),
+                  card_fee: Yup.number()
+                    .nullable()
+                    .moreThan(-1, "card fee must be less than 0"),
+                  max_amount: Yup.number()
+                    .nullable()
+                    .moreThan(-1, "max amount must be less than 0"),
+                  effective_months: Yup.number()
+                    .nullable()
+                    .moreThan(-1, "effective month must be less than 0"),
+                  unit_cost: Yup.number().required(
+                    "unit cost must be required"
+                  ),
+                })}
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                  handleSubmit(values, { setSubmitting, resetForm });
+                }}
+              >
+                {(props) => (
+                  <form className="mt-5">
+                    {activeTab === "Personal" && (
+                      <div className="w-full grid grid-cols-12 gap-4">
+                        <div className="col-span-6">
+                          <Input
+                            isRequired
+                            size="sm"
+                            type="number"
+                            label="Product Code"
+                            name="nik"
+                            variant="bordered"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.nik}
+                          />
+                          {props.touched.nik && props.errors.nik ? (
+                            <div className="text-sm text-primary font-semibold">
+                              {props.errors.nik}
+                            </div>
+                          ) : null}
+                        </div>
+
+                        <div className="col-span-6">
+                          <Input
+                            isRequired
+                            size="sm"
+                            label="Full Name"
+                            name="full_name"
+                            variant="bordered"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.full_name}
+                          />
+                          {props.touched.full_name && props.errors.full_name ? (
+                            <div className="text-sm text-primary font-semibold">
+                              {props.errors.full_name}
+                            </div>
+                          ) : null}
+                        </div>
+                      </div>
+                    )}
+                    {activeTab === "Family" && <></>}
+
+                    <Button
+                      color="primary"
+                      radius="sm"
+                      className="w-full mt-4 font-semibold hover:bg-secondary"
+                      type="submit"
+                      isLoading={loading ? true : false}
+                    >
+                      Register
+                    </Button>
+                    <p className="mt-5 text-center text-sm text-gray-500">
+                      Already have an account?{" "}
+                      <span
+                        className="font-semibold leading-6 text-black-500 hover:text-red-500 cursor-pointer"
+                        onClick={() => {
+                          router.push("/");
+                        }}
+                      >
+                        Login
+                      </span>
+                    </p>
+                  </form>
+                )}
+              </Formik>
+            </>
           </div>
         </div>
       </div>
